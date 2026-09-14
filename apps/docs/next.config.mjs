27705006c0d133c96cc7ed5e305c61558e5be4af
Yekta <168995838+yekta100110@users.noreply.github.com>
2026-09-15 01:52:48 +0300

    Simplify next.config.mjs by removing configs
    Removed TypeScript and ESLint configurations from next.config.mjs.

--- apps/docs/next.config.mjs
+++ apps/docs/next.config.mjs
@@ -1,18 +1,11 @@
-import nextra from "nextra";
+ import nextra from "nextra";
+
 
 const withNextra = nextra({});
 
-const nextConfig = {
-  reactStrictMode: true,
-  
-  typescript: {
-    // Derleme anında TypeScript hatalarının build'i bozmasını engeller
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    // ESLint hatalarının build'i bozmasını engeller
-    ignoreDuringBuilds: true,
-  },
-};
 
-export default withNextra(nextConfig);
+export default withNextra({
+
+  reactStrictMode: true,
+
+}); 
